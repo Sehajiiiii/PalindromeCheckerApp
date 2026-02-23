507f26e069f6a7265dd286bc6f4924e425c539bd
bloodybelly <writetobloodybelly@gmail.com>
2026-02-23 09:26:06 +0530

    UC4: Case insensitive palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,11 +4,7 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
         displayWelcomeMessage();
-<<<<<<< HEAD
-        checkPalindromeUsingMiddle();
-=======
-        checkHardcodedPalindrome();
->>>>>>> feature/UC2
+        checkPalindrome();
     }
 
     public static void displayWelcomeMessage() {
@@ -17,49 +13,31 @@ public class PalindromeCheckerApp {
         System.out.println("=================================");
     }
 
-<<<<<<< HEAD
-    public static void checkPalindromeUsingMiddle() {
+    public static void checkPalindrome() {
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a word: ");
         String word = scanner.nextLine();
 
-        int length = word.length();
+        String processedWord = word.toLowerCase();
+
+        int length = processedWord.length();
         boolean isPalindrome = true;
 
-        // Compare characters from start and end moving toward middle
         for (int i = 0; i < length / 2; i++) {
-
-            if (word.charAt(i) != word.charAt(length - 1 - i)) {
+            if (processedWord.charAt(i) != processedWord.charAt(length - 1 - i)) {
                 isPalindrome = false;
                 break;
             }
         }
 
         if (isPalindrome) {
-=======
-    public static void checkHardcodedPalindrome() {
-
-        String word = "madam";   // Hardcoded value
-        String reversed = "";
-
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
-        }
-
-        System.out.println("Word: " + word);
-
-        if (word.equals(reversed)) {
->>>>>>> feature/UC2
-            System.out.println("It is a Palindrome.");
+            System.out.println(word + " is a Palindrome.");
         } else {
-            System.out.println("It is NOT a Palindrome.");
+            System.out.println(word + " is NOT a Palindrome.");
         }
-<<<<<<< HEAD
 
         scanner.close();
-=======
->>>>>>> feature/UC2
     }
 }
