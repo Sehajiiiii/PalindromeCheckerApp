255fd742c10013d54c4e384cb61e878ccb1fe58b
bloodybelly <writetobloodybelly@gmail.com>
2026-02-23 09:23:01 +0530

    UC2: Hardcoded palindrome using reverse logic

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,12 +2,30 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
         displayWelcomeMessage();
+        checkHardcodedPalindrome();
     }
 
     public static void displayWelcomeMessage() {
         System.out.println("=================================");
         System.out.println("     PALINDROME CHECKER APP     ");
         System.out.println("=================================");
-        System.out.println("This application checks whether a word is a palindrome.");
+    }
+
+    public static void checkHardcodedPalindrome() {
+
+        String word = "madam";   // Hardcoded value
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        System.out.println("Word: " + word);
+
+        if (word.equals(reversed)) {
+            System.out.println("It is a Palindrome.");
+        } else {
+            System.out.println("It is NOT a Palindrome.");
+        }
     }
 }
